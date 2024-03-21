53046fa9678f84c1a22e743705c0c1fcd6ece02c
sreenaths <somarajapuram@gmail.com>
2024-03-21 14:17:39 -0700

    Verbiage changes in download script

--- scripts/download_source.sh
+++ scripts/download_source.sh
@@ -1,6 +1,7 @@
-mkdir ./source
+# Download Spider zip into ./source
 
 DOC_ID="1TqleXec_OykOYFREKKtschzY29dUcVAQ"
+
 wget \
     --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$( \
         wget --quiet \
@@ -10,4 +11,5 @@ wget \
         sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p' \
     )&id=$DOC_ID" \
     -O ./source/spider.zip
+
 rm -rf /tmp/cookies.txt
