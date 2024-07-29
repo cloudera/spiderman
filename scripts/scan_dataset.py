ebe571ad74ef78964496978e89800e384a7e98a3
Sreenath Somarajapuram <somarajapuram@gmail.com>
2024-07-29 12:35:07 -0700

    Improved stats display - Show seperate db list for test and train

--- scripts/scan_dataset.py
+++ scripts/scan_dataset.py
@@ -3,7 +3,7 @@
 from argparse import ArgumentParser
 
 from core.dataset import DatasetDir
-from utils.print import print_dict
+from utils.print import print_bar
 from utils.filesystem import read_str, read_csv
 
 
@@ -17,31 +17,29 @@ def _get_counts(dataset: DatasetDir, query_file_path: str):
         schema = read_str(dataset.path_to_schema_file(db_name))
         tables_count += schema.count('CREATE TABLE')
 
-    return len(queries), len(db_names), tables_count
+    return len(queries), tables_count, db_names
 
 def print_stats(dataset: DatasetDir) -> dict:
-    train_queries, train_dbs, train_tables = _get_counts(dataset, dataset.path_to_train_queries_file())
-    test_queries, test_dbs, test_tables = _get_counts(dataset, dataset.path_to_test_queries_file())
+    train_queries, train_tables, train_db_names = _get_counts(dataset,
+                                                              dataset.path_to_train_queries_file())
+    test_queries, test_tables, test_db_names = _get_counts(dataset,
+                                                           dataset.path_to_test_queries_file())
 
-    db_names = dataset.get_db_names()
+    print_bar("Dataset Stats")
 
-    stats = {
-        "Train queries": train_queries,
-        "Tables used in train queries": train_tables,
-        "DBs with train queries": train_dbs,
+    print("Train queries:", train_queries)
+    print("Tables used in train queries:", train_tables)
+    print(f"DBs with train queries ({len(train_db_names)}):", ", ".join(train_db_names))
 
-        "Test queries": test_queries,
-        "Tables used in test queries": test_tables,
-        "DBs with test queries": test_dbs,
+    print("\nTest queries:", test_queries)
+    print("Tables used in test queries:", test_tables)
+    print(f"DBs with test queries ({len(test_db_names)}):", ", ".join(test_db_names))
 
-        "Total queries": train_queries + test_queries,
-        "Total tables": train_tables + test_tables,
-        "Total DBs": train_dbs + test_dbs,
+    print("\nTotal queries:", train_queries + test_queries)
+    print("Total tables:", train_tables + test_tables)
+    print("Total DBs:", len(train_db_names) + len(test_db_names))
 
-        "DB names": ", ".join(db_names)
-    }
-
-    print_dict(stats, label="Dataset Stats")
+    print_bar()
 
     #TODO: Get top 10 DBs and tables with maximum rows as part of stats
 
--- scripts/utils/print.py
+++ scripts/utils/print.py
@@ -1,10 +1,5 @@
 BAR_WIDTH = 70
 
-def print_dict(d, label: str = ""):
-    prefix = f"\n--- {label} " if label else ""
+def print_bar(label: str = ""):
+    prefix = f"--- {label} " if label else ""
     print(prefix + "-" * (BAR_WIDTH - len(prefix)))
-
-    for key, value in d.items():
-        print(f"{key}: {value}")
-
-    print("-" * BAR_WIDTH)
