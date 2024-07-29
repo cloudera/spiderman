85fd5c946f82d2f9c5642b4c14992ffc7f7b2616
Sreenath Somarajapuram <somarajapuram@gmail.com>
2024-07-29 16:28:59 -0700

    Validate Dataset - Loading queries using pandas

--- scripts/validate_queries.py
+++ scripts/validate_queries.py
@@ -1,11 +1,15 @@
-"""Try running train & test queries on the target database, and validate they can be successfully executed"""
+"""
+Try running train & test queries on the target database,
+and validate they can be successfully executed
+"""
 
 import sys
 from alive_progress import alive_bar
 
+import pandas as pd
+
 from core.target_db import TargetDB
 from core.dataset import DatasetDir
-from utils.filesystem import read_csv
 from utils.args import get_args
 
 
@@ -13,45 +17,36 @@ args = get_args("Validate successful execution of all queries on the target data
 
 dataset = DatasetDir(args.dialect)
 
-def get_db_sql_map(query_file_path: str) -> dict[str, list[str]]:
-    db_sql_map: dict[str, list[str]] = {}
+def execute_queries(query_file_path: str):
+    """Execute all queries from file at query_file_path"""
 
-    queries = read_csv(query_file_path)[1:]
-    for query in queries:
-        db_name = query[0]
-        sql = query[2]
+    queries_df = pd.read_csv(query_file_path)
+    db_names = sorted(set(queries_df['database']))
 
-        if db_name not in db_sql_map:
-            db_sql_map[db_name] = []
-
-        db_sql_map[db_name].append(sql)
-
-    return db_sql_map
-
-def execute_sql(db_sql_map: dict[str, list[str]]):
-    with alive_bar(len(db_sql_map.keys())) as progress:
-        for db_name in db_sql_map:
+    with alive_bar(len(db_names)) as progress:
+        for db_name in db_names:
+            db_queries_df = queries_df[queries_df['database'] == db_name]
+            query_count = len(db_queries_df)
 
             progress.text(f">> DB: {db_name}")
             with TargetDB(args.url, db_name) as db:
-                sqls = db_sql_map[db_name]
-
-                count = len(sqls)
-                for idx, sql in enumerate(sqls):
-                    progress.text(f">>>>> DB: {db_name} | {idx} | Query: {idx}/{count}")
+                for idx, query in db_queries_df.iterrows():
                     try:
-                        db.execute(sql)
+                        progress.text(f">>>> DB: {db_name} | Query: {idx}/{query_count}")
+                        db.execute(query["sql"])
                     except Exception as e:
                         print(e)
-                        print("Details: ", db_name, idx, sql)
+                        print("Details: ", db_name, idx, query["question"], query["sql"])
                         sys.exit()
 
             progress() # pylint: disable=not-callable
 
+    print(f"Executed {len(queries_df)} queries.")
+
 print("Executing train queries...")
-execute_sql(get_db_sql_map(dataset.path_to_train_queries_file()))
+execute_queries(dataset.path_to_train_queries_file())
 
 print("Executing test queries...")
-execute_sql(get_db_sql_map(dataset.path_to_test_queries_file()))
+execute_queries(dataset.path_to_test_queries_file())
 
 print("Validation successful.")
